8dd3aa0d19e05d656f354d200a077f26dfc6f026
gustavo <gustavosilvadky02@gmail.com>
2023-01-11 19:34:07 -0300

    create project hr-worker

---
+++ hr-worker/hr-worker/src/test/java/com/workers/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workers.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
